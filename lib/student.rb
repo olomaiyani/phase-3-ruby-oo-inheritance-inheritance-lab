009bc5472739a970a31164198ed8afa33029884e
Nana <nanaolomaiyani@gmail.com>
2022-10-26 19:51:35 +0300

    assignment completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
 class Student 
 
+    require_relative './user.rb'
+
+    class Student < User
+    
+        attr_accessor :knowledge
+    
+        def initialize
+            @knowledge = []
+        end
+    
+        def learn(my_string)
+            @knowledge << my_string
+        end
+    
+        def self.knowledge
+            @knowledge
+        end
 end
